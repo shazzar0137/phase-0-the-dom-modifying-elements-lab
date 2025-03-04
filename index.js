cdabc56d71bd5406aa32d61a30f3b92284ff4c70
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-04 08:46:13 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Daniel is the champion";
+document.body.appendChild(newHeader)
